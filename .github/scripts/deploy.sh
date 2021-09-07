3fd40fa6a166c092530b51a5e9e4120759d7f342
Andrew Lee <andrewjkes@hotmail.com>
2021-09-07 23:36:59 +0800

    update

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd functions
+npm ci
+
+if [ -z "${FIREBASE_TOKEN}" ]; then
+    echo "FIREBASE_TOKEN is missing"
+    exit 1
+fi
+
+firebase deploy \
+    --token ${FIREBASE_TOKEN} \
+    --only functions
