dafdb405424050ad80a5e5fd17f0c2eced1ce02e
Andrew Lee <andrewjkes@hotmail.com>
2021-09-07 23:04:06 +0800

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,14 @@
-import * as functions from "firebase-functions";
+import { ApolloServer, gql } from 'apollo-server-express';
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import 'firebase-functions';
+
+admin.initializeApp(functions.config().firebase);
+const cors = require("cors");
+
+const app = express();
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
